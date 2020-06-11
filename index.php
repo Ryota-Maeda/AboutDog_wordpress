cf093c60da6e1b054b863c68e3888db0feee4f19
前田凌汰 <>
2020-06-11 14:52:07 +0900

    header footer 分割済み

--- index.php
+++ index.php
@@ -1,26 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AboutDog</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
 
-    <?php wp_head(); ?>
-</head>
-<body>
-  <div class="container">
-    <header>
-      <h1>About Dog</h1>
-      <nav>
-        <ul class="header-right">
-          <li>Top</li>
-          <li>Latest</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
+<?php get_header(); ?>
 
     <div>
       <img src="<?php echo get_template_directory_uri(); ?>/img/dog.jpg" alt="dog" class="top-image">
@@ -86,11 +65,9 @@
         </ul>
 
       </div>
-      
     </section>
-
   </div>
 
-  <?php wp_footer(); ?>
-</body>
-</html>
+  <nav></nav>
+
+  <?php get_footer(); ?>
